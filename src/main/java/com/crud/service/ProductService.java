8d97a6e06ed42fa329f470a7866fcc7817bc7c4b
ialexjx <akshathrx6393@gmail.com>
2024-01-05 23:29:34 +0530

    No changes

--- src/main/java/com/crud/service/ProductService.java
+++ src/main/java/com/crud/service/ProductService.java
@@ -5,6 +5,8 @@ import com.crud.repo.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -14,4 +16,10 @@ public class ProductService {
     public void saveProduct(){
         productRepository.save(new Product());
     }
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+
 }
