514e28fc30f3f18edfbe1ff34c4906b3645cb377
ialexjx <akshathrx6393@gmail.com>
2024-01-09 23:33:21 +0530

    No changes

--- src/main/java/com/crud/controller/CrudController.java
+++ src/main/java/com/crud/controller/CrudController.java
@@ -1,56 +1,56 @@
-//package com.crud.controller;
-//
-//import com.crud.model.Product;
-//import com.crud.service.ProductService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class CrudController {
-//
-//    @Autowired
-//    ProductService productService;
-//
-//    @GetMapping("/hello")
-//    public String getHelloMessage() {
-//        return "Hello, this is a Spring Boot example!";
-//    }
-//
-//    @PostMapping("/addProduct")
-//    public void saveProduct(@RequestBody Product product){
-//         productService.saveProduct(product);
-//    }
-//
-//    @PostMapping("/addProducts")
-//    public void saveAllProducts(@RequestBody List<Product> productList){
-//        productService.saveAllProducts(productList);
-//    }
-//
-//    @GetMapping("/getAllProducts")
-//    public List<Product> getAllProducts(){
-//        return productService.getAllProducts();
-//    }
-//
-//    @GetMapping("/getProductById/{id}")
-//    public Product getProductById(@PathVariable int id){
-//        return productService.getProductById(id);
-//    }
-//
-//    @GetMapping("/getProductByName")
-//    public List<Product> getProductByName(@RequestParam String productName){
-//        return productService.getProductByName(productName);
-//    }
-//
-//    @PutMapping("/updateProduct")
-//    public Product updateProduct(@RequestBody Product product){
-//        return productService.updateProduct(product);
-//    }
-//
-//    @DeleteMapping("/deleteProduct/{id}")
-//    public void deleteProduct(@PathVariable int id){
-//        productService.deleteProduct(id);
-//    }
-//}
+package com.crud.controller;
+
+import com.crud.model.Product;
+import com.crud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CrudController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/hello")
+    public String getHelloMessage() {
+        return "Hello, this is a Spring Boot example!";
+    }
+
+    @PostMapping("/addProduct")
+    public void saveProduct(@RequestBody Product product){
+         productService.saveProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public void saveAllProducts(@RequestBody List<Product> productList){
+        productService.saveAllProducts(productList);
+    }
+
+    @GetMapping("/getAllProducts")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/getProductById/{id}")
+    public Product getProductById(@PathVariable int id){
+        return productService.getProductById(id);
+    }
+
+    @GetMapping("/getProductByName")
+    public List<Product> getProductByName(@RequestParam String productName){
+        return productService.getProductByName(productName);
+    }
+
+    @PutMapping("/updateProduct")
+    public Product updateProduct(@RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @DeleteMapping("/deleteProduct/{id}")
+    public void deleteProduct(@PathVariable int id){
+        productService.deleteProduct(id);
+    }
+}
